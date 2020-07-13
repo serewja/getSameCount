514131633ea308f9eede6f1e799ba74f3e565dea
Sergei Shinkarenko <serewja@gmail.com>
2020-07-13 20:54:50 +0500

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,8 @@
-import f from "../dist/index.js"
+import getSameCount from "../dist/index.js"
 
-test('testname', () => {
-    
+test('test', () => {
+  expect(getSameCount([1, 3, 2, 2], [3, 1, 1, 2, 5])).toBe(3);
+  expect(getSameCount([1, 4, 4], [4, 8, 4])).toBe(1);
+  expect(getSameCount([1, 10, 3], [10, 100, 35, 1])).toBe(2);
+  expect(getSameCount([], [])).toBe(0);
 });
